784498da2443143fd8346632ef5f96592463a5bb
abdulhakim14 <abdulhakimiubat@gmail.com>
2023-11-19 22:14:14 +0600

    update header page

--- admin/add_category.php
+++ admin/add_category.php
@@ -80,46 +80,19 @@ else
         <svg class="circular" viewBox="25 25 50 50">
 			<circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" /> </svg>
     </div>
+
     <!-- Main wrapper  -->
+
     <div id="main-wrapper">
+
         <!-- header header  -->
-        <div class="header">
-            <nav class="navbar top-navbar navbar-expand-md navbar-light">
-                             
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="index.php">
-                       
-                        <span><b>ORFOS</b></span>
-                    </a>
-                </div>
-             
-                <div class="navbar-collapse">
-                    <!-- toggle and nav items -->
-                    <ul class="navbar-nav mr-auto mt-md-0">
-                        <!-- This is  -->
-                        <li class="nav-item"> <a class="nav-link nav-toggler hidden-md-up text-muted  " href="javascript:void(0)"><i class="mdi mdi-menu"></i></a> </li>
-                        <li class="nav-item m-l-10"> <a class="nav-link sidebartoggler hidden-sm-down text-muted  " href="javascript:void(0)"><i class="ti-menu"></i></a> </li>
-                     
-                       
-                    </ul>
+        
+        <?php
+        include("./header.php");
+        ?>
 
-                    <!-- User profile -->
-                    <ul class="navbar-nav my-lg-0">
-
-                        <!-- Profile -->
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle text-muted  " href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="images/users/5.jpg" alt="user" class="profile-pic" /></a>
-                            <div class="dropdown-menu dropdown-menu-right animated zoomIn">
-                                <ul class="dropdown-user">
-                                    <li><a href="logout.php"><i class="fa fa-power-off"></i> LogOut</a></li>
-                                </ul>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
         <!-- End header header -->
+
         <!-- Left Sidebar  -->
         <div class="left-sidebar">
             <!-- Sidebar scroll-->
