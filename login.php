c75320a3845c2af212f53a7e69eb144d65e5483f
abdulhakim14 <abdulhakimiubat@gmail.com>
2023-11-21 16:31:01 +0600

    update the registration page

--- login.php
+++ login.php
@@ -59,68 +59,40 @@ if(isset($_POST['submit']))   // if button is submit
 }
 ?>
   
-<!-- Form Mixin-->
-<!-- Input Mixin-->
-<!-- Button Mixin-->
-<!-- Pen Title-->
 
-<!--header starts-->
-<header id="header" class="header-scroll top-header headrom">
-            <!-- .navbar -->
-            <nav class="navbar navbar-dark">
-               <div class="container">
-                  <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
-                  <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="images/food-picky-logo.png" alt=""> </a>
-                  <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
-                     <ul class="nav navbar-nav">
-							<li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
-                            <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Restaurants <span class="sr-only"></span></a> </li>
-                            
-							<?php
-						if(empty($_SESSION["user_id"]))
-							{
-								echo '<li class="nav-item"><a href="login.php" class="nav-link active">Login</a> </li>
-							  <li class="nav-item"><a href="registration.php" class="nav-link active">Sign Up</a> </li>';
-							}
-						else
-							{
-									
-									
-										echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">Your Orders</a> </li>';
-									echo  '<li class="nav-item"><a href="logout.php" class="nav-link active">LogOut</a> </li>';
-							}
 
-						?>
-							 
-                        </ul>
-                  </div>
-               </div>
-            </nav>
-        <!-- /.navbar -->
-</header>
+	<!--header starts-->
 
-<div class="pen-title">
-  <h1>Login Page</h1>
-</div>
-<!-- Form Module-->
-<div class="module form-module">
-  <div class="toggle">
-   
-  </div>
-  <div class="form">
-    <h2>Login to your account</h2>
-	  <span style="color:red;"><?php echo $message; ?></span> 
-   <span style="color:green;"><?php echo $success; ?></span>
-    <form action="" method="post">
-      <input type="text" placeholder="Username"  name="username"/>
-      <input type="password" placeholder="Password" name="password"/>
-      <input type="submit" id="buttn" name="submit" value="Login" />
-    </form>
-  </div>
-  
-  <div class="cta">Not registered?<a href="registration.php" style="color:#f30;"> Create an account</a></div>
-</div>
-  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+		<?php
+            include("header.php");
+        ?>
+
+     <!--header End-->>
+	 
+
+		<div class="pen-title">
+		<h1>Login Page</h1>
+		</div>
+		
+		<!-- Form Module-->
+		<div class="module form-module">
+		<div class="toggle">
+		
+		</div>
+		<div class="form">
+			<h2>Login to your account</h2>
+			<span style="color:red;"><?php echo $message; ?></span> 
+		<span style="color:green;"><?php echo $success; ?></span>
+			<form action="" method="post">
+			<input type="text" placeholder="Username"  name="username"/>
+			<input type="password" placeholder="Password" name="password"/>
+			<input type="submit" id="buttn" name="submit" value="Login" />
+			</form>
+		</div>
+		
+		<div class="cta">Not registered?<a href="registration.php" style="color:#f30;"> Create an account</a></div>
+		</div>
+		<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 
   
 
