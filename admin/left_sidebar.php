1423375327d9f74cf78ca240ecf862326aa16e03
abdulhakim14 <abdulhakimiubat@gmail.com>
2023-12-06 11:53:38 +0600

    add report page

--- admin/left_sidebar.php
+++ admin/left_sidebar.php
@@ -46,9 +46,13 @@
 								  
                             </ul>
                         </li>
+                        
                         <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-file" aria-hidden="true"></i><span class="hide-menu">Reports</span></a>
                             <ul aria-expanded="false" class="collapse">
-								<li><a href="all_report.php">All Reports</a></li>
+								<li><a href="report_delivered.php">Delivered</a></li>
+                                <li><a href="report_dispatch.php">Dispatch</a></li>
+                                <li><a href="report_cancel.php">Cancel</a></li>
+                                <li><a href="report_ontheway.php">On the Way</a></li>
 								  
                             </ul>
                         </li>
