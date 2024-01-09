b0cd653b0cecf521383ea7b629953733e5f7edaa
abdulhakim14 <abdulhakimiubat@gmail.com>
2024-01-09 19:54:10 +0600

    update

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
             <nav class="navbar navbar-dark d-flex">
                 <div class="container">
                     <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
-                    <a class="navbar-brand" href="index.php"> <strong>ORFOS</strong> </a>
+                    <a class="navbar-brand" href="index.php"><img class="img-rounded" src="images/orfos-logo.png" alt=""> <strong>ORFOS</strong> </a>
                     <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
                         <ul class="nav navbar-nav">
                             <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
